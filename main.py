3645668443cc399e78d1c1e850b49ca7fc322a9f
krkim0921 <chemestry100@gmail.com>
2022-08-15 20:01:29 +0900

    add httpresopnse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -35,7 +35,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code= status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000)
@@ -44,5 +44,9 @@ def create_posts(post: Post):
 
 @app.get("/posts/{id}")
 def get_post(id: int):
+
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail= f"post with id: {id} was not found")
     return {'post_detail': post}
