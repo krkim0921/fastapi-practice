1b3198dff127f9e73d27fe17eef67d59f82e4e27
krkim0921 <chemestry100@gmail.com>
2022-08-16 19:25:20 +0900

    add update, delete

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+
 my_posts = [
     {'title': 'title of post 1',
     'content': 'content of post1',
@@ -26,6 +27,11 @@ def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 @app.get('/')
 def root():
@@ -50,3 +56,26 @@ def get_post(id: int):
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
                             detail= f"post with id: {id} was not found")
     return {'post_detail': post}
+
+@app.delete('/posts/{id}', status_code= status.HTTP_204_NO_CONTENT)
+def delete_post(id:int):
+    index = find_index_post(id)
+    
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f" post with {id} does not exist")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id:int, post:Post):
+
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f" post with {id} does not exist")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {'data': post_dict}
